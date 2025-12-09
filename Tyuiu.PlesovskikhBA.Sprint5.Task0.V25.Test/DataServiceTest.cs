54b208d5f1d7204444ce29642dca7af4338e4464
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-09 23:56:41 +0500

    Add project files.

---
+++ Tyuiu.PlesovskikhBA.Sprint5.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Tyuiu.PlesovskikhBA.Sprint5.Task0.V25.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint5.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+
+            int x = 3;
+            string path = ds.SaveToFileTextData(x);
+
+            Assert.IsTrue(File.Exists(path));
+
+            string result = File.ReadAllText(path);
+
+            Assert.AreEqual("9,037", result);
+        }
+    }
+}
