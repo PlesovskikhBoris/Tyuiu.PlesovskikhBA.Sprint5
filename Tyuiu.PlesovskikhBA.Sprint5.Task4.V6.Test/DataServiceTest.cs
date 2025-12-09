b4114195570193ec4b27949f7cc8adb2520952b5
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-10 02:21:50 +0500

    Task4

---
+++ Tyuiu.PlesovskikhBA.Sprint5.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Tyuiu.PlesovskikhBA.Sprint5.Task4.V6.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint5.Task4.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V6.txt";
+
+            FileInfo fl = new FileInfo(path);
+            bool fle = fl.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fle);
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint5.Task4.V6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using Tyuiu.PlesovskikhBA.Sprint5.Task4.V6.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint5.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask4V6.txt";
+            Console.WriteLine("Данные находятся в файле : " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
