119c84e5ffd8ae3a63b1235de789468410a264af
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-10 05:39:48 +0500

    Task7

---
+++ Tyuiu.PlesovskikhBA.Sprint5.Task7.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Tyuiu.PlesovskikhBA.Sprint5.Task7.V27.Lib;
+namespace Tyuiu.PlesovskikhBA.Sprint5.Task7.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckExists()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask7V27.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool exists = fileInfo.Exists;
+            Assert.IsTrue(exists);
+        }
+    }
+}
